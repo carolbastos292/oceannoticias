fa1f4253920e868c7b969ae1ad6de6c04a42328a
Carol Bastos <acbl.ads@uea.edu.br>
2018-08-25 18:48:17 -0400

    Definida as rotas do CRUD

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,10 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+/*Route::get('/admin/noticias',['as'->'admin.noticias','uses'=>'Admin\NoticiaController@index']);
+Route::get('/admin/noticias/cadastrar',['as'->'admin.noticias.cadastrar','uses'=>'Admin\NoticiaController@cadastrar']);
+Route::post('/admin/noticias/salvar',['as'->'admin.noticias.salvar','uses'=>'Admin\NoticiaController@salvar']);
+Route::get('/admin/noticias/editar/{id}',['as'->'admin.noticias.editar','uses'=>'Admin\NoticiaController@editar']);
+Route::put('/admin/noticias/atualizar/{id}',['as'->'admin.noticias.atualizar','uses'=>'Admin\NoticiaController@atualizar']);
+Route::get('/admin/noticias/deletar/{id}',['as'->'admin.noticias.deletar','uses'=>'Admin\NoticiaController@deletar']);*/
