03b3c7446e871182509d30aadfad7938bdaef947
Carol Bastos <acbl.ads@uea.edu.br>
2018-08-25 19:13:27 -0400

    Incluido biblioteca Materialize

---
+++ resources/views/layout/_includes/ropape.blade.php
@@ -0,0 +1,10 @@
+    <!-- Compiled and minified JavaScript -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0-rc.2/js/materialize.min.js"></script>
+    <script src="https://code.jquery.com/jquery-2.2.4.min.js" integrity="sha256-BbhdlvQf/xTY9gja0Dq3HiwQF8LaCRTXxZKRutelT44=" crossorigin="anonymous"></script>
+    <script type="text/javascript">
+    	$(document).ready(function(){
+    		Materialize.updateTexFields();
+    	});
+    </script>
+  </body>
+</html>
