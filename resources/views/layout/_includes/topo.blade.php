bf19a63964cfbcb4b13141211b97096171837b14
Carol Bastos <acbl.ads@uea.edu.br>
2018-08-25 19:29:05 -0400

    Incluido navbar responsiva

--- resources/views/layout/_includes/topo.blade.php
+++ resources/views/layout/_includes/topo.blade.php
@@ -10,18 +10,18 @@
     <!--Let browser know website is optimized for mobile-->
     <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
   </head>
-
-  <nav>
-    <div class="nav-wrapper">
-      <a href="#!" class="brand-logo">OceanNoticias</a>
-      <a href="#" data-activates="mobile-demo" class="button-collapse"><i class="material-icons">menu</i></a>
-      <ul class="right hide-on-med-and-down">
-        <li><a href="sass.html">Cadastrar Noticias</a></li>
-      </ul>
-      <ul class="side-nav" id="mobile-demo">
-        <li><a href="sass.html">Cadastrar Noticias</a></li>
-      </ul>
-    </div>
-  </nav>
-
   <body>
+    <header>
+      <nav>
+      <div class="nav-wrapper">
+        <a href="#!" class="brand-logo">OceanNoticias</a>
+        <a href="#" data-activates="mobile-demo" class="button-collapse"><i class="material-icons">menu</i></a>
+        <ul class="right hide-on-med-and-down">
+          <li><a href="sass.html">Cadastrar Noticias</a></li>
+        </ul>
+        <ul class="side-nav" id="mobile-demo">
+          <li><a href="sass.html">Cadastrar Noticias</a></li>
+        </ul>
+      </div>
+    </nav>
+  </header>
