3d2b980bc2072bac4100f37ef55a487636556259
Carol Bastos <acbl.ads@uea.edu.br>
2018-08-25 21:18:23 -0400

    Ajustes no rodape e no topo

---
+++ resources/views/layout/_includes/rodape.blade.php
@@ -0,0 +1,13 @@
+    <!-- Jquery -->
+    <script type="text/javascript" src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
+     <!-- Compiled and minified JavaScript -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/js/materialize.min.js"></script>
+    
+    <script type="text/javascript">
+    	 $(document).ready(function() {
+            Materialize.updateTextFields();
+         });
+
+    </script>
+  </body>
+</html>
