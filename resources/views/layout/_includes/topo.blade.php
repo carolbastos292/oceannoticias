0d7fefb0df73ee43d35228b35df7550ef87ea011
Carol Bastos <acbl.ads@uea.edu.br>
2018-08-25 21:27:14 -0400

    Incluidas rotas do cadastrar noticia no template

--- resources/views/layout/_includes/topo.blade.php
+++ resources/views/layout/_includes/topo.blade.php
@@ -10,20 +10,19 @@
     <!--Let browser know website is optimized for mobile-->
     <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
   </head>
-
   <body>
     <header>
     <nav>
     <div class="nav-wrapper deep-orange">
-      <a href="#!" class="brand-logo">OceanNoticias</a>
+      <a href="#!" class="brand-logo">Ocean Noticias</a>
       <a href="#" data-activates="mobile" class="button-collapse"><i class="material-icons">menu</i></a>
       <ul class="right hide-on-med-and-down">
         <li><a href="/">Home</a></li>
-        <li><a href="#">Cadastrar Noticias</a></li>
+        <li><a href="{{route('admin.noticias.cadastrar')}}">Cadastrar Noticias</a></li>
       </ul>
       <ul class="side-nav" id="mobile">
         <li><a href="/">Home</a></li>
-        <li><a href="#">Cadastrar Noticias</a></li>
+        <li><a href="{{route('admin.noticias.cadastrar')}}">Cadastrar Noticias</a></li>
       </ul>
     </div>
   </nav>
