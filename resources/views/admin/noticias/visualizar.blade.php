f3d2c445b26a9d7f40c98aa5dd16eb987143ffac
Carol Bastos <acbl.ads@uea.edu.br>
2018-08-27 01:25:45 -0400

    Adicionado botão flutuante com funcão editar e apagar

--- resources/views/admin/noticias/visualizar.blade.php
+++ resources/views/admin/noticias/visualizar.blade.php
@@ -11,6 +11,17 @@
 		<h5 class="grey-text darken-1" align="right">{{ date( 'd/m/Y' , strtotime($registro->created_at))}}</h5> 
 		<h2 class="">{{$registro->titulo}}</h2>
         <h5 class="">{{$registro->descricao}}</h5>
+
+		<!--Botão de ação flutuante -->
+        <div class="fixed-action-btn">
+		  <a class="btn-floating btn-large blue pulse">
+		    <i class="large material-icons">menu</i>
+		  </a>
+		  <ul>
+		    <li><a class="btn-floating red" href="{{route('admin.noticias.editar',$registro->id)}}"><i class="material-icons">mode_edit</i></a></li>
+		    <li><a class="btn-floating amber darken-1" href="{{route('admin.noticias.deletar',$registro->id)}}"><i class="material-icons">delete</i></a></li>
+		  </ul>
+		</div>
 	</div>
 
 	<script type="text/javascript">
